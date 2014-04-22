99c0591663837023aa38e03bfb99c06fbcd548d1
Jangal <djangal@BOS-Scout-DJangal.local>
2014-04-22 14:26:31 -0400

    First commit for test automation

---
+++ src/models/Requisition_Models.py
@@ -0,0 +1,72 @@
+import datetime as dt
+from datetime import datetime, time, timedelta
+now = dt.datetime.now()
+
+TBE_REQ1 = {
+    "openDate": now.strftime("%m/%d/%Y"),
+    "title": "robot_TBE_BH_Scout_Test1",
+    "location": '39',
+    "jobCat": '114',
+    "status": '2',
+    "numOpen": '2',
+    "duration": '1',
+    "abc": 'CUSTOM_935'    
+}
+
+TBE_REQ2 = {
+    "openDate": now.strftime("%m/%d/%Y"),
+    "title": "robot_TBE_BH_Scout_Test2",
+    "location": '73',
+    "jobCat": '122',
+    "status": '2',
+    "numOpen": '2',
+    "duration": '1',
+    "abc": 'CUSTOM_935'    
+}
+
+TBE_REQ3 = {
+    "openDate": now.strftime("%m/%d/%Y"),
+    "title": "robot_TBE_BH_Scout_Test3",
+    "location": '49',
+    "jobCat": '121',
+    "status": '2',
+    "numOpen": '2',
+    "duration": '1',
+    "abc": 'CUSTOM_935'    
+}
+
+TBE_REQ4 = {
+    "openDate": now.strftime("%m/%d/%Y"),
+    "title": "robot_TBE_PCR_Scout_Test1",
+    "location": '61',
+    "jobCat": '116',
+    "status": '2',
+    "numOpen": '2',
+    "duration": '1',
+    "abc": 'CUSTOM_935'    
+}
+
+TBE_REQ5 = {
+    "openDate": now.strftime("%m/%d/%Y"),
+    "title": "robot_TBE_PCR_Scout_Test2",
+    "location": '38',
+    "jobCat": '115',
+    "status": '2',
+    "numOpen": '2',
+    "duration": '1',
+    "abc": 'CUSTOM_935'    
+}
+
+TBE_REQ6 = {
+    "openDate": now.strftime("%m/%d/%Y"),
+    "title": "robot_TBE_PCR_Scout_Test3",
+    "location": '57',
+    "jobCat": '113',
+    "status": '2',
+    "numOpen": '2',
+    "duration": '1',
+    "abc": 'CUSTOM_935'    
+}
+
+
+
