2495aa9df21602a1b59dc8052d3159b0c11328b3
Jangal <djangal@BOS-Scout-DJangal.local>
2014-09-12 14:31:55 -0400

    Modified as per code review comments

---
+++ src/models/Candidate_Rejection.py
@@ -0,0 +1,12 @@
+TBE_Rejection_Reasons = {
+    "Candidate_Qualifications": '0',  
+    "Placement_Fee": '1',
+    "Other": '2'
+}
+
+TBE_Rejection_Comments = {
+    "Candidate_Qualifications": "Testing TBE Rejection for Candidate Qualifications",  
+    "Placement_Fee": "Testing TBE Rejection for Placement Fee",
+    "Other": "Testing TBE Rejection for Other reasons"
+}
+
