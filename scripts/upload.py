d36e3192c3e0eeebd81480ae2818b9d19dd9d8fa
Jangal <djangal@BOS-Scout-DJangal.local>
2015-07-15 11:03:54 -0400

    Updated path to keyfile

--- scripts/upload.py
+++ scripts/upload.py
@@ -14,5 +14,6 @@ remote_dir_root = "/home/ubuntu/report/Scout/%s/%s" % (outDir, inDir)
 #remote_dir_root = "/home/ubuntu/report/Scout/%s" % os.environ['USER']
 remote_report_dir ="%s/%s" % (remote_dir_root, dirname)
 mkdir_cmd = "mkdir -p %s" % (remote_report_dir)
-res = os.system('ssh -i /Users/djangal/.ssh/yzhaotalentdrive.pem ubuntu@qa01.talentfilter.biz "%s" ' % mkdir_cmd)
-res2 = os.system('scp -i /Users/djangal/.ssh/yzhaotalentdrive.pem %s/* ubuntu@qa01.talentfilter.biz:%s' %(inDir, remote_report_dir))
+res = os.system('ssh -i ~/.ssh/yzhaotalentdrive.pem ubuntu@qa01.talentfilter.biz "%s" ' % mkdir_cmd)
+res2 = os.system('scp -i ~/.ssh/yzhaotalentdrive.pem %s/* ubuntu@qa01.talentfilter.biz:%s' %(inDir, remote_report_dir))
+
