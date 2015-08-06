0c8c2ac2cf6637d90fc4a40c12fd14d179d29807
gregpscout <gregp@goscoutgo.com>
2015-08-06 15:45:08 -0400

    run_so.bat - removed old calling line

---
+++ scripts/run_so.bat
@@ -0,0 +1,9 @@
+pybot -i WRITEINI          --output so_writeini.xml     --report so_writeini.html        ..\src\features\sendouts\Sendouts_Test_Cases.txt
+#pybot -i IMPSRCH           --output so_impsearch.xml    --report so_impsearch.html       ..\src\features\sendouts\Sendouts_Test_Cases.txt
+#pybot -i SUBMIT             --output so_submit.xml       --report so_submit.html          ..\src\features\sendouts\Sendouts_Test_Cases.txt
+#pybot -i SHARE             --output so_share.xml       --report so_share.html           ..\src\features\sendouts\Sendouts_Test_Cases.txt
+#pybot -i ACCEPT             --output so_accept.xml       --report so_accept.html           ..\src\features\sendouts\Sendouts_Test_Cases.txt
+#pybot -i REJECT             --output so_reject.xml       --report so_reject.html           ..\src\features\sendouts\Sendouts_Test_Cases.txt
+#pybot -i TESTLINKS          --output so_testlinks.xml   --report so_testlinks.html         ..\src\features\sendouts\Sendouts_Test_Cases.txt
+
+#pybot -i PLACE               --output so_place.xml      --report so_place.html              ..\src\features\sendouts\Sendouts_Test_Cases.txt
